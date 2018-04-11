9d1b0ee60c13766f9132fff635f9869df82ae78a
seasongao <seasongao@126.com>
2018-04-11 15:09:32 +0800

    添加了动态代理

--- app/src/main/java/com/javabasicdemo/MainActivity.java
+++ app/src/main/java/com/javabasicdemo/MainActivity.java
@@ -2,9 +2,8 @@ package com.javabasicdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import java.util.LinkedList;
+import designModel.dynamicProxy.ProxyTest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String s=StringReverse.reverse("123466789");
-        Log.d("tag","反转后的字符串="+s);
-        LinkedList list=new LinkedList();
-        list.add("");
+//        String s=StringReverse.reverse("123466789");
+//        Log.d("tag","反转后的字符串="+s);
+//        LinkedList list=new LinkedList();
+//        list.add("");
+//
+        //动态代理测试
+        new ProxyTest().login();
 
-        AboutThread.run();
     }
 }
